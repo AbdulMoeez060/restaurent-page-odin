6dd71e36860a365e380391ffd6991faf676ea96a
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-03 16:32:36 +0500

    add simple nav strucuture

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import './styles/main.css';
 
+const website = (()=>{
+    var content = document.querySelector('.content');
+
+    function addNav(){
+
+        var nav = document.createElement('div');
+        nav.classList.add('nav');
+        var logo = document.createElement('p');
+        logo.textContent = "Odin's Inn"
+
+        var list = document.createElement('ul');
+        var links = ['Home','Menu','About']
+        
+
+        for (let i = 0; i < 3; i++) {
+            let line = document.createElement('li');
+            let button =  document.createElement('button');
+            button.classList.add(links[i])
+            let text = document.createTextNode(links[i]);
+            button.appendChild(text);
+            line.appendChild(button);
+            list.appendChild(line);
+        }
+        
+        
+
+
+        nav.appendChild(logo);
+        nav.appendChild(list);
+
+        content.appendChild(nav);
+        
+    }
+
+    addNav()
+    
+
+})()
