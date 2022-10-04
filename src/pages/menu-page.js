372e03da6166fbfa6f1f198fe15e95b31d31982f
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-04 19:24:12 +0500

    add menu items

---
+++ src/pages/menu-page.js
@@ -0,0 +1,70 @@
+
+const menuItems = [
+    {
+        category: 'Pizza',
+        items:[
+            {
+                name: 'Chicken Tikka',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Bihari Kebab',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Chicken Fajita',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Pepporoni',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Tex-Mex',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Odin Special',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+        ]
+    },
+    {
+        category: 'Burgers',
+        items: [
+            {
+                name: 'Cheesy Chicken',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Beast Burger',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Daddy of All Burgers',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Odins Special',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+        ]
+    },
+    {
+        category :'Drinks',
+        items:[
+            {
+                name: 'Coke',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Sprite',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                name: 'Fanta',
+                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+        ]
+    }
+]
